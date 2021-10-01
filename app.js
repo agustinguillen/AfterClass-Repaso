d64d623a01627f45e26e83038d634906d67688e5
aguillenloro <a.guillen@loro.com.co>
2021-10-01 16:58:44 -0300

    AfterClass Repaso

--- app.js
+++ app.js
@@ -1,110 +1,99 @@
 //------------------APP DE PELICULAS - AFTECLASS DOM--------------------//
+const baseUrl = "https://currency-exchange.p.rapidapi.com/exchange";
+let calculos = [];
+let fecha = new Date();
+let fechaFormato = fecha.toLocaleDateString("es-AR");
+let id = Number(localStorage.getItem("proximoID")) || 0;
 
-//Array de peliculas
-let peliculas = [
-    {id:1, nombre: 'Star Wars', genero: 'ciencia ficcion', anio: 1977, atp: true, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625508101/afterclass/ahT4ObS7XKedQkOSpGr1wQ97aKA_s86oek.jpg'},
-    {id:2, nombre: 'Nobody', genero: 'accion', anio: 2021, atp: false, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625508101/afterclass/ddO5a3tMPpQutSDQO1bESgLWadB_nsr7h6.jpg'},
-    {id:3, nombre: 'Mortal Kombat', genero: 'aventura', anio: 2021, atp: false, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625508102/afterclass/t2GbiMJfO8txoihdJC8RsfK1ds7_e9o7v4.jpg'},
-    {id:4, nombre: 'Interstellar', genero: 'ciencia ficcion', anio: 2014, atp: false, img: 'https://res.cloudinary.com/vaporbox/image/upload/v1625508102/afterclass/nrSaXF39nDfAAeLKksRCyvSzI2a_hvgguc.jpg'},
-    {id:5, nombre: 'The Big Lebowsky', genero: 'comedia', anio: 1998, atp: false, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625508101/afterclass/EJFkJD9BH400jfzKz3W5xLYHQa_ic29jw.jpg'},
-    {id:6, nombre: 'Mulan', genero: 'aventura', anio: 2020, atp: true, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625508101/afterclass/72XYwdIugXnjHxS6upI6U0yeTBU_lk2u9w.jpg' },
-    {id:7, nombre: 'A Quiet Place', genero: 'terror', anio: 2018, atp: false, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625508101/afterclass/5pairI20KJs1SEa4Z8yAsPR779m_a9uwch.jpg'},
-    {id:8, nombre: 'The Exorcist', genero: 'terror', anio: 1973, atp: false, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625508103/afterclass/rQU3sOBms4WA7iFAqBCtbMXgaB8_ynkkix.jpg'},
-    {id:9, nombre: 'SpongeBob', genero: 'animacion', anio: 2020, atp: true, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625508102/afterclass/fi2pg2mtAZwhq3qVuAs6PztjnHT_hbdv35.jpg'},
-    {id:10, nombre: 'Luca', genero: 'animacion', anio: 2020, atp: true, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625540511/afterclass/pr06RihHOGE3waZQx5fs2WYUdwr_a7dvh2.jpg'},
-    {id:11, nombre: 'Spirit', genero: 'animacion', anio: 2021, atp: true, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625540564/afterclass/mptEtOkw906BJuD9rRUKG5sr5E9_s43aw0.jpg'},
-    {id:12, nombre: 'Monster Inc.', genero: 'animacion', anio: 2001, atp: true, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625540669/afterclass/jTE3BLVCpYaR71L2U8SHV22HvEr_fbtiu6.jpg'},
-    {id:13, nombre: 'Blade Runner', genero: 'ciencia ficcion', anio: 1982, atp: false, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625540759/afterclass/k7tpmwwSqwJ6l1f1FqDMnM7x5c2_tgukok.jpg'},
-    {id:14, nombre: 'Joker', genero: 'drama', anio: 2019, atp: false, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625540959/afterclass/v0eQLbzT6sWelfApuYsEkYpzufl_e85kyq.jpg'},
-    {id:15, nombre: 'Rey León', genero: 'animacion', anio: 1994, atp: true, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625541034/afterclass/b0MxU37dNmMwKtoPVYPKOZSIrIn_xx4f3x.jpg'},
-    {id:16, nombre: 'Jurassic Park', genero: 'ciencia ficcion', anio: 1993, atp: false, img:'https://res.cloudinary.com/vaporbox/image/upload/v1625541181/afterclass/1r8TWaAExHbFRzyqT3Vcbq1XZQb_jotm6u.jpg'},
-]
+class Exchange {
+  constructor(id, moneda1, moneda2, valor1, valor2, fecha) {
+    this.id = id;
+    this.moneda1 = moneda1;
+    this.moneda2 = moneda2;
+    this.valor1 = valor1;
+    this.valor2 = valor2;
+    this.fecha = fecha;
+  }
+}
 
-let edad= Number(prompt('Cual es tu edad?'))
-let preguntar = confirm('Querés filtrar por año?')
-let atp //boolean
-const body = document.body
-//Div de id="resultado"
-let resultado = document.getElementById('resultado')
-resultado.setAttribute('class', 'col-lg-10 offset-1 d-flex flex-wrap justify-content-start text-center')
-//Elemento título
-let titulo = document.createElement('h1')
-titulo.setAttribute('class', 'text-center mt-5')
-body.prepend(titulo)
+function getCurrency(e) {
+  e.preventDefault();
+  if ($("#moneda1").val() !== "" || $("#moneda2").val() !== "") {
+    const settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": `${baseUrl}?from=${$("#moneda1").val()}&to=${$(
+        "#moneda2"
+      ).val()}&q=1.0`,
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "dd8c9b746cmsh16c2dbc5f41e74cp1765d2jsnd8b477815ac0",
+        "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+      },
+    };
 
-//Función que crea las tarjetas HTML de cada película de mi array peliculas
-function crearTarjetasHTML(filtradas){
-    let peliculasFiltradas = filtradas
-    for(const pelicula of peliculasFiltradas){
-        resultado.innerHTML += `<div id="movie${pelicula.id}" class="card">
-                                    <img src=${pelicula.img} class="card-img-top">
-                                    <div class="card-body">
-                                        <h5 class="card-title">${pelicula.nombre}</h5>
-                                        <p class="card-text">Genero: ${pelicula.genero} <br> Año: ${pelicula.anio} <br> ATP: ${pelicula.atp}</p>
-                                    </div>
-                                </div>`
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      let resultado = Number($("#valor1").val()) * Number(response);
+      $("#valor2").val(resultado);
+      $("#valor2").addClass("input-yellow");
+    });
+  }
+}
+
+function mostrarResultado() {
+  let moneda1 = $("#moneda1").val();
+  let moneda2 = $("#moneda2").val();
+  let valor1 = Number($("#valor1").val());
+  let valor2 = Number($("#valor2").val());
+  let resultado = new Exchange(id, moneda1, moneda2, valor1, valor2, fechaFormato);
+  calculos.push(resultado);
+  localStorage.setItem("calculos", JSON.stringify(calculos));
+  $("#resultados")
+    .append(
+      `<div id="${id}" class="resultado"><strong>${valor1} ${moneda1} es igual a ${valor2} ${moneda2}</strong> <span>${fechaFormato}</span><button class="eliminar">X</button></div>`
+    )
+    .hide()
+    .fadeIn(500);
+  $(".eliminar").click((e) => eliminarResultado(e));
+  id++;
+  localStorage.setItem("proximoID", id);
+}
+
+function cargarResultados() {
+  let guardados = JSON.parse(localStorage.getItem("calculos"));
+  console.log(guardados);
+  if (guardados !== undefined && guardados !== null) {
+    calculos = guardados;
+    for (const guardado of guardados) {
+      $("#resultados")
+        .append(
+          `<div id="${guardado.id}" class="resultado"><strong>${guardado.valor1} ${guardado.moneda1} es igual a ${guardado.valor2} ${guardado.moneda2}</strong> <span>${fechaFormato}</span><button class="eliminar">X</button></div>`
+        )
+        .hide()
+        .fadeIn(500);
     }
-}
-//Función para dar estilo a las tarjetas y para modificar dependiendo si es mayor o menor de 13 años
-function darEstilo(atp){
-    if(!atp){
-        body.setAttribute('style', 'background-color: #000A5C; color: whitesmoke')
-    }else if(atp){
-        body.setAttribute('style', 'background-color: #1CCF00')
-    }
-    let cards = document.getElementsByClassName('card')
-    for(const card of cards){
-        card.setAttribute('style', 'width: 13rem; background-color: black; color: whitesmoke; margin: 2.2rem')
-    }
-
-   
+    $(".eliminar").click((e) => eliminarResultado(e));
+  }
 }
 
-//Función para filtrar por el año que ingrese el usuario
-function filtrarPorAnio(){
-    removeAllChildNodes(resultado)
-    let filtro = prompt('Desde que año te gustaría buscar?').toString()
-    let peliculasFiltradas = peliculas.filter(pelicula => pelicula.anio >= filtro)
-    crearTarjetasHTML(peliculasFiltradas)
-    titulo.textContent = `Peliculas posteriores a ${filtro}`
+function eliminarResultado(e) {
+  let elemento = e.target.parentNode;
+  $(`#${elemento.id}`).fadeOut(500, function () {
+    $(this).remove();
+  });
+  console.log(e.target.parentNode.id);
+  let nuevoCalculos = calculos.filter(
+    (item) => item.id !== Number(elemento.id)
+  );
+  calculos = nuevoCalculos;
+  localStorage.setItem("calculos", JSON.stringify(calculos));
 }
 
-//Funcion para borrar todos los nodos child (hijos) del elemento parent que le pase por el atributo. firstChild siempre me devuelve el primer child del nodo que especifique
-function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-}
-
-//Si la persona es mayor de 13 o tiene 13 años cambio el valor de atp, cambio el título y le paso mi array de peliculas completo (si es mayor) o filtrado por atp (si es menor)
-if(edad >= 13){
-    atp=false
-    titulo.textContent= 'Todas las películas'
-    crearTarjetasHTML(peliculas)
-
-    
-
-}else{
-    atp=true
-    titulo.textContent= 'Películas para vos'
-    peliculas = peliculas.filter(pelicula => pelicula.atp === true)
-    crearTarjetasHTML(peliculas)
-}
-
-//Si en el confirm() preguntar da ok me ejecuta la función filtrar por año
-if(preguntar){
-    filtrarPorAnio()
-}
-
-darEstilo(atp)
-
-if(!atp){
-    //Seleccione mediante el id que cree dinámicamente en la función crearTarjetasHTML para modificar el color de una tarjeta en específico
-    document.getElementById('movie3').style.backgroundColor = 'red'
-}
-
-/*removeChild() borra el nodo child que yo le pase del parent que seleccioné en este caso body*/
-//body.removeChild(resultado)
-
-/*remove() elimina el nodo que seleccioné en este caso resultado*/
-//resultado.remove()
+$(document).ready(function () {
+  cargarResultados();
+  $("#formulario").submit((e) => {
+    getCurrency(e);
+  });
+  $("#guardar").click(mostrarResultado);
+});
